4f9a06be7c9d46c0bf1aba14bec27994a1b7dc89
Dichiara25 <edclrcq@gmail.com>
2024-03-21 19:08:30 +0100

    Modularized use case section

--- src/app/components/structure/home/Landing.tsx
+++ src/app/components/structure/home/Landing.tsx
@@ -10,6 +10,71 @@ import Developer from "../../../assets/developer.png"
 import { routes } from "@/app/utils/routes";
 import Feedbacks from "./Feedbacks";
 import How from "./How";
+import { StaticImageData } from "next/image";
+
+interface UseCase {
+    title: string,
+    content: string,
+    image: StaticImageData,
+    alt: string,
+    callToAction: string,
+    link: string,
+    reverse: boolean,
+    quote: string
+}
+
+function UseCases() {
+    const useCases: UseCase[] = [
+        {
+            title: "Study smarter, not harder",
+            content: `${process.env.NEXT_PUBLIC_APP_NAME} uses AI to automatically transform lecture videos and reading materials into study aids and assessments. Literally for free.`,
+            image: Student,
+            alt: "A smiling student studying his lessons",
+            callToAction: "Start generating for free",
+            link: routes.cook,
+            reverse: false,
+            quote: ""
+        },
+        {
+            title: "Streamline corporate training",
+            content: `Using ${process.env.NEXT_PUBLIC_APP_NAME}, upgrade your training materials by converting online meetings, corporate documentation and instructional videos into interactive quizzes.`,
+            image: Meeting,
+            alt: "A smiling student studying his lessons",
+            callToAction: "Select your plan",
+            link: routes.pricing,
+            reverse: true,
+            quote: ""
+        },
+        {
+            title: "Adapted to your workflow",
+            content: `${process.env.NEXT_PUBLIC_APP_NAME} exposes a REST API so you can embed your quiz generation use cases right into your workflows. By providing an interface out of the box, ${process.env.NEXT_PUBLIC_APP_NAME} relieves you from having to create your own.`,
+            image: Developer,
+            alt: "A happy developer in front of an IDE",
+            callToAction: "Get your API key",
+            link: routes.api,
+            reverse: false,
+            quote: ""
+        },
+    ]
+
+    return useCases
+        .map((useCase: UseCase, index: number) => (
+            <div key={index}>
+                <ScrollAppear>
+                    <Paragraph
+                        title={useCase.title}
+                        content={useCase.content}
+                        image={useCase.image}
+                        alt={useCase.alt}
+                        link={useCase.link}
+                        callToAction={useCase.callToAction}
+                        reverse={useCase.reverse}
+                        quote={useCase.quote}
+                    />
+                </ScrollAppear>
+            </div>
+        ))
+}
 
 export default function Landing() {
     return <div className={styles.main}>
@@ -19,39 +84,7 @@ export default function Landing() {
         <ScrollAppear>
             <How />
         </ScrollAppear>
-        <ScrollAppear>
-            <Paragraph
-                title={"Study smarter, not harder"}
-                content={`${process.env.NEXT_PUBLIC_APP_NAME} uses AI to automatically transform lecture videos and reading materials into study aids and assessments. Literally for free.`}
-                image={Student}
-                alt={"A smiling student studying his lessons"}
-                callToAction={"Start generating for free"}
-                link={routes.cook}
-                reverse={true}
-            />
-        </ScrollAppear>
-        <ScrollAppear>
-            <Paragraph
-                title={"Streamline corporate training"}
-                content={`Using ${process.env.NEXT_PUBLIC_APP_NAME}, upgrade your training materials by converting online meetings, corporate documentation and instructional videos into interactive quizzes.`}
-                image={Meeting}
-                alt={"A business woman attending an online meeting with a happy face"}
-                callToAction={"Select your plan"}
-                link={routes.pricing}
-                reverse={false}
-            />
-        </ScrollAppear>
-        <ScrollAppear>
-            <Paragraph
-                title={"Adapted to your workflow"}
-                content={`${process.env.NEXT_PUBLIC_APP_NAME} exposes a REST API so you can embed your quiz generation use cases right into your workflows. By providing an interface out of the box, ${process.env.NEXT_PUBLIC_APP_NAME} relieves you from having to create your own.`}
-                image={Developer}
-                alt={"A happy developer in front of an IDE"}
-                callToAction={"Get your API key"}
-                link={routes.api}
-                reverse={true}
-            />
-        </ScrollAppear>
+        <UseCases />
         <ScrollAppear>
             <Feedbacks />
         </ScrollAppear>
--- src/app/components/structure/home/Paragraph.tsx
+++ src/app/components/structure/home/Paragraph.tsx
@@ -2,8 +2,13 @@ import Image from 'next/image'
 import paragraphStyles from './Paragraph.module.css'
 import bodyStyles from "../Body.module.css"
 
-function Illustration(props: {image: any, alt: string}) {
-    return <Image src={props.image} width={500} alt={props.alt} />
+function Illustration(props: {image: any, alt: string, quote: string, reverse: boolean}) {
+    return <div style={{position: "relative"}}>
+        <Image src={props.image} width={500} alt={props.alt} />
+        {props.quote && <div style={props.reverse ? {left: "10px"} : {right: "10px"}} className={paragraphStyles.quote}>
+            {props.quote}
+        </div>}
+    </div>
 }
 
 function Text(props: {title: string, content: string, callToAction: string, link: string,}) {
@@ -16,16 +21,16 @@ function Text(props: {title: string, content: string, callToAction: string, link
     </div>
 }
 
-export default function Paragraph(props: {title: string, content: string, image: any, alt: string, callToAction: string, link: string, reverse: boolean}) {
+export default function Paragraph(props: {title: string, content: string, image: any, alt: string, callToAction: string, link: string, reverse: boolean, quote: string}) {
     return <div className={paragraphStyles.main}>
         {props.reverse ? <>
-                <Illustration image={props.image} alt={props.alt} />
+                <Illustration image={props.image} alt={props.alt} quote={props.quote} reverse={props.reverse} />
                 <Text title={props.title} content={props.content} callToAction={props.callToAction} link={props.link} />
             </>
             :
             <>
                 <Text title={props.title} content={props.content} callToAction={props.callToAction} link={props.link} />
-                <Illustration image={props.image} alt={props.alt} />
+                <Illustration image={props.image} alt={props.alt} quote={props.quote} reverse={props.reverse} />
             </>
         }
     </div>
